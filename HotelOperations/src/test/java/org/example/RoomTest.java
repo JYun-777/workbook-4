41ad832625560a061f1167f3383f29f47781b7cc
JonY <jonathanwyun@gmail.com>
2025-05-07 00:10:49 -0700

    Room Unit tests begun

---
+++ HotelOperations/src/test/java/org/example/RoomTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    public void checkIn_shouldMakeRoomOccupiedDirty(){
+
+    }
+
+    @Test
+    public void checkOut_shouldClearRoom(){
+
+    }
+
+    public void cleanRoom_shouldMakeRoomNotDirty{
+        //
+    }
+}
